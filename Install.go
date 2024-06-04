3b1b940b2cb2f4e637e6d56962fa7512802eb9f8
Codepulze <151552809+EvilBytecode@users.noreply.github.com>
2024-06-04 20:21:53 +0100

    Create Install.go
    create installer

---
+++ Install.go
@@ -0,0 +1,17 @@
+@Echo off
+title DOWNLOADING MODULES
+go get github.com/EvilBytecode/GoDefender/AntiVirtualization/TriageDetection
+go get github.com/EvilBytecode/GoDefender/AntiVirtualization/MonitorMetrics
+go get github.com/EvilBytecode/GoDefender/AntiVirtualization/VirtualboxDetection
+go get github.com/EvilBytecode/GoDefender/AntiVirtualization/VMWareDetection
+go get github.com/EvilBytecode/GoDefender/AntiVirtualization/KVMCheck
+go get github.com/EvilBytecode/GoDefender/AntiVirtualization/UsernameCheck
+go get github.com/EvilBytecode/GoDefender/AntiDebug/IsDebuggerPresent
+go get github.com/EvilBytecode/GoDefender/AntiDebug/RemoteDebugger
+go get github.com/EvilBytecode/GoDefender/AntiDebug/pcuptime
+go get github.com/EvilBytecode/GoDefender/AntiDebug/CheckBlacklistedWindowsNames
+go get github.com/EvilBytecode/GoDefender/AntiDebug/RunningProcesses
+go get github.com/EvilBytecode/GoDefender/AntiDebug/ParentAntiDebug
+go get github.com/EvilBytecode/GoDefender/AntiDebug/KillBadProcesses
+go get github.com/EvilBytecode/GoDefender/Process/CriticalProcess
+pause
