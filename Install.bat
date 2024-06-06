2a578739d4a42c8db54aefadfc71f45bceafcd8a
Codepulze <151552809+EvilBytecode@users.noreply.github.com>
2024-06-06 16:31:08 +0100

    Update Install.bat
    update to v1.0.5

--- Install.bat
+++ Install.bat
@@ -1,7 +1,7 @@
 @Echo off
 title DOWNLOADING MODULES
 go mod init GoDefender
-go get github.com/EvilBytecode/GoDefender@v1.0.3
+go get github.com/EvilBytecode/GoDefender@v1.0.5
 go get github.com/EvilBytecode/GoDefender/AntiVirtualization/TriageDetection
 go get github.com/EvilBytecode/GoDefender/AntiVirtualization/MonitorMetrics
 go get github.com/EvilBytecode/GoDefender/AntiVirtualization/VirtualboxDetection
