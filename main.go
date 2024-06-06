bcae98713b9d71b8b40690f7ea52a04d94b92c9e
0xF <106026079+zwinplayer64@users.noreply.github.com>
2024-06-06 21:20:13 +0300

    Update main script

--- main.go
+++ main.go
@@ -3,79 +3,78 @@ package main
 import (
 	"fmt"
 	"os"
-	// Anti-Virtualization
-	"github.com/EvilBytecode/GoDefender/AntiVirtualization/TriageDetection"
-	"github.com/EvilBytecode/GoDefender/AntiVirtualization/MonitorMetrics"
-	"github.com/EvilBytecode/GoDefender/AntiVirtualization/VirtualboxDetection"
-	"github.com/EvilBytecode/GoDefender/AntiVirtualization/VMWareDetection"
-	"github.com/EvilBytecode/GoDefender/AntiVirtualization/KVMCheck"
-	"github.com/EvilBytecode/GoDefender/AntiVirtualization/UsernameCheck"
-	"github.com/EvilBytecode/GoDefender/AntiVirtualization/USBCheck"
-	"github.com/EvilBytecode/GoDefender/AntiVirtualization/RecentFileActivity"
 
+	// Anti-Virtualization
+	hypervisor "github.com/EvilBytecode/GoDefender/AntiVirtualization/Hypervisor"
+	kvmcheck "github.com/EvilBytecode/GoDefender/AntiVirtualization/KVMCheck"
+	"github.com/EvilBytecode/GoDefender/AntiVirtualization/MonitorMetrics"
+	"github.com/EvilBytecode/GoDefender/AntiVirtualization/RecentFileActivity"
+	triagecheck "github.com/EvilBytecode/GoDefender/AntiVirtualization/TriageDetection"
+	"github.com/EvilBytecode/GoDefender/AntiVirtualization/USBCheck"
+	usernamecheck "github.com/EvilBytecode/GoDefender/AntiVirtualization/UsernameCheck"
+	vmcheck "github.com/EvilBytecode/GoDefender/AntiVirtualization/VMCheck"
 
 	// Anti-Debug
+	blacklistcheck "github.com/EvilBytecode/GoDefender/AntiDebug/CheckBlacklistedWindowsNames"
 	"github.com/EvilBytecode/GoDefender/AntiDebug/IsDebuggerPresent"
-	"github.com/EvilBytecode/GoDefender/AntiDebug/RemoteDebugger"
+	remotedebuggercheck "github.com/EvilBytecode/GoDefender/AntiDebug/RemoteDebugger"
+	runningprocesses "github.com/EvilBytecode/GoDefender/AntiDebug/RunningProcesses"
 	"github.com/EvilBytecode/GoDefender/AntiDebug/pcuptime"
-	"github.com/EvilBytecode/GoDefender/AntiDebug/CheckBlacklistedWindowsNames"
-	"github.com/EvilBytecode/GoDefender/AntiDebug/RunningProcesses"
-	//"github.com/EvilBytecode/GoDefender/AntiDebug/ParentAntiDebug"
-	"github.com/EvilBytecode/GoDefender/AntiDebug/KillBadProcesses"
-	"github.com/EvilBytecode/GoDefender/AntiDebug/UserAntiAntiDebug"
-	"github.com/EvilBytecode/GoDefender/AntiDebug/InternetCheck"
 
+	//"github.com/EvilBytecode/GoDefender/AntiDebug/ParentAntiDebug"
+	"github.com/EvilBytecode/GoDefender/AntiDebug/InternetCheck"
+	processkiller "github.com/EvilBytecode/GoDefender/AntiDebug/KillBadProcesses"
+	userantiantidebug "github.com/EvilBytecode/GoDefender/AntiDebug/UserAntiAntiDebug"
 	// Process Related
 	//"github.com/EvilBytecode/GoDefender/Process/CriticalProcess"
 )
 
 func main() {
-	/* 
-	ANTIDEBUG
-	-----------
-	- IsDebuggerPresent
-	- RemoteDebugger
-	- PC Uptime Check
-	- Running Proccesses Count
-	- Check blacklisted windows
-	- KillBlacklisted Proceseses
-	- Parent AntiDebug
+	/*
+	   Anti-Debug
+	   -----------
+	   - IsDebuggerPresent
+	   - RemoteDebugger
+	   - PC Uptime Check
+	   - Running Proccesses Count
+	   - Check blacklisted windows
+	   - KillBlacklisted Proceseses
+	   - Parent AntiDebug
 	*/
 	RecentFileActivity.RecentFileActivityCheck()
 	USBCheck.PluggedIn()
 	userantiantidebug.AntiAntiDebug()
-        IsDebuggerPresent.IsDebuggerPresent()
+	IsDebuggerPresent.IsDebuggerPresent()
 	remotedebuggercheck.RemoteDebugger()
 	pcuptime.CheckUptime(1200)
 	runningprocesses.CheckRunningProcessesCount(50)
-        blacklistcheck.CheckBlacklistedWindows()
+	blacklistcheck.CheckBlacklistedWindows()
 	//parentantidebug.ParentAntiDebug()
 	processkiller.KillProcesses()
 
-	/* 
-	AntiVirulization
-	----------------
-	- Triage Check
-	- VMWare Check
-	- Anti KVM
-	- Username Check
-	- 
+	/*
+	   Anti-Virtulization
+	   ----------------
+	   - Triage Check
+	   - VM Gpu check
+	   - Anti KVM
+	   - Username Check
+	   - Hypervisor CPUID bit check
 	*/
 
 	InternetCheck.CheckConnection()
 	triagecheck.TriageCheckDebug()
 	MonitorMetrics.IsScreenSmall()
-	VirtualboxDetection.GraphicsCardCheck()
-	fmt.Println("Debug Check: VirtualBox isnt present")
-	VMWare.GraphicsCardCheck()
-	fmt.Println("Debug Check: VMWare isnt present")
+	vmcheck.GraphicsCardCheck()
+	fmt.Println("No VM GPUs present")
 	if kvmcheck.CheckForKVM() {
 		os.Exit(-1)
 	}
+	hypervisor.CheckHypervisorBit()
 	usernamecheck.CheckForBlacklistedNames()
 	fmt.Println("IF YOURE HERE YOU PASSED LOL")
 	/*
-	EXTRA THINGS NOW:
+	   EXTRA THINGS NOW:
 	*/
 	//programutils.SetDebugPrivilege() this is for devs who plan on continuing
 	//programutils.SetProcessCritical() // this automatically gets the SeDebugPrivillige
