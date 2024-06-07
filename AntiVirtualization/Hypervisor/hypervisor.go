c73825e0a775100605506ed53273aee59541d3c2
0xF <106026079+zwinplayer64@users.noreply.github.com>
2024-06-07 15:43:26 +0300

    update check + exit

--- AntiVirtualization/Hypervisor/hypervisor.go
+++ AntiVirtualization/Hypervisor/hypervisor.go
@@ -12,8 +12,8 @@ import (
 func CheckHypervisorBit() bool {
 	// the hypervisor bit is always 0 on real cpus
 	const hypervisor = cpuid.HYPERVISOR
-	if hypervisor != 0 && cpuid.HYPERV == 0 {
-		fmt.Printf("The hypervisor cpuid bit isn't 0 (%x)\n", hypervisor)
+	if hypervisor != 0 {
+		fmt.Printf("The hypervisor cpuid bit isn't 0 (0x%x)\n", hypervisor)
 		return true
 	}
 	fmt.Println("Hypervisor bit is 0.")
--- main.go
+++ main.go
@@ -5,26 +5,26 @@ import (
 	"os"
 
 	// Anti-Virtualization
+	"github.com/EvilBytecode/GoDefender/AntiDebug/InternetCheck"
 	hypervisor "github.com/EvilBytecode/GoDefender/AntiVirtualization/Hypervisor"
 	kvmcheck "github.com/EvilBytecode/GoDefender/AntiVirtualization/KVMCheck"
 	"github.com/EvilBytecode/GoDefender/AntiVirtualization/MonitorMetrics"
-	"github.com/EvilBytecode/GoDefender/AntiVirtualization/RecentFileActivity"
 	triagecheck "github.com/EvilBytecode/GoDefender/AntiVirtualization/TriageDetection"
-	"github.com/EvilBytecode/GoDefender/AntiVirtualization/USBCheck"
 	usernamecheck "github.com/EvilBytecode/GoDefender/AntiVirtualization/UsernameCheck"
 	vmcheck "github.com/EvilBytecode/GoDefender/AntiVirtualization/VMCheck"
 
 	// Anti-Debug
 	blacklistcheck "github.com/EvilBytecode/GoDefender/AntiDebug/CheckBlacklistedWindowsNames"
 	"github.com/EvilBytecode/GoDefender/AntiDebug/IsDebuggerPresent"
+	processkiller "github.com/EvilBytecode/GoDefender/AntiDebug/KillBadProcesses"
+	parentantidebug "github.com/EvilBytecode/GoDefender/AntiDebug/ParentAntiDebug"
 	remotedebuggercheck "github.com/EvilBytecode/GoDefender/AntiDebug/RemoteDebugger"
 	runningprocesses "github.com/EvilBytecode/GoDefender/AntiDebug/RunningProcesses"
-	"github.com/EvilBytecode/GoDefender/AntiDebug/pcuptime"
-
-	//"github.com/EvilBytecode/GoDefender/AntiDebug/ParentAntiDebug"
-	"github.com/EvilBytecode/GoDefender/AntiDebug/InternetCheck"
-	processkiller "github.com/EvilBytecode/GoDefender/AntiDebug/KillBadProcesses"
 	userantiantidebug "github.com/EvilBytecode/GoDefender/AntiDebug/UserAntiAntiDebug"
+	"github.com/EvilBytecode/GoDefender/AntiDebug/pcuptime"
+	"github.com/EvilBytecode/GoDefender/AntiVirtualization/RecentFileActivity"
+	"github.com/EvilBytecode/GoDefender/AntiVirtualization/USBCheck"
+	//"github.com/EvilBytecode/GoDefender/AntiDebug/ParentAntiDebug"
 	// Process Related
 	//"github.com/EvilBytecode/GoDefender/Process/CriticalProcess"
 )
@@ -49,9 +49,8 @@ func main() {
 	pcuptime.CheckUptime(1200)
 	runningprocesses.CheckRunningProcessesCount(50)
 	blacklistcheck.CheckBlacklistedWindows()
-	//parentantidebug.ParentAntiDebug()
+	parentantidebug.ParentAntiDebug()
 	processkiller.KillProcesses()
-
 	/*
 	   Anti-Virtulization
 	   ----------------
@@ -70,7 +69,10 @@ func main() {
 	if kvmcheck.CheckForKVM() {
 		os.Exit(-1)
 	}
-	hypervisor.CheckHypervisorBit()
+	if hypervisor.CheckHypervisorBit() {
+		fmt.Println("If you have the Hyper-V feature enabled on your system, please disable it.\nHow to: https://www.guidingtech.com/how-to-disable-hyper-v-windows/")
+		os.Exit(-1)
+	}
 	usernamecheck.CheckForBlacklistedNames()
 	fmt.Println("IF YOURE HERE YOU PASSED LOL")
 	/*
