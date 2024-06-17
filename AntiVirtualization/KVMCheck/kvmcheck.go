ea0615e98d8477ca61d4d5fc1a4c67a0b77a7396
Codepulze <151552809+EvilBytecode@users.noreply.github.com>
2024-06-17 10:15:24 +0100

    Update kvmcheck.go
    Revert / making folders for other detections

--- AntiVirtualization/KVMCheck/kvmcheck.go
+++ AntiVirtualization/KVMCheck/kvmcheck.go
@@ -1,36 +1,17 @@
 package KVMCheck
 
 import (
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
-	"strings"
 )
 
 // CheckForKVM checks for the presence of Kernel-based Virtual Machine (KVM) components.
 // It returns true if KVM components are detected, otherwise false.
-// ON 17/06/2024 (Parralers and Qemu Detection Added.)
 func CheckForKVM() (bool, error) {
-	badDriversList := []string{"balloon.sys", "netkvm.sys", "vioinput", "viofs.sys", "vioser.sys", "qemu-ga", "qemuwmi", "prl_sf", "prl_tg", "prl_eth"}
-	systemFolder := filepath.Join(os.Getenv("SystemRoot"), "System32")
-
-	files, err := ioutil.ReadDir(systemFolder)
-	if err != nil {
-		log.Printf("Error accessing system folder: %v", err)
-		return false, err
-	}
-
-	for _, file := range files {
-		for _, badDriver := range badDriversList {
-			if strings.Contains(file.Name(), badDriver) {
-				return true, nil
-			}
-		}
-	}
+	badDriversList := []string{"balloon.sys", "netkvm.sys", "vioinput", "viofs.sys", "vioser.sys"}
 	for _, driver := range badDriversList {
-		driverPattern := filepath.Join(systemFolder, driver)
-		files, err := filepath.Glob(driverPattern)
+		files, err := filepath.Glob(filepath.Join(os.Getenv("SystemRoot"), "System32", driver))
 		if err != nil {
 			log.Printf("Error accessing system files for %s: %v", driver, err)
 			continue
@@ -39,6 +20,5 @@ func CheckForKVM() (bool, error) {
 			return true, nil
 		}
 	}
-
 	return false, nil
 }
