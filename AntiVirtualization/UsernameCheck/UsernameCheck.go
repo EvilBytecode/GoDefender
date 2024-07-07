72f9031c2a632ba494b1b23a5025f4014237d850
Codepulze <151552809+EvilBytecode@users.noreply.github.com>
2024-07-07 09:36:57 +0100

    Update UsernameCheck.go
    Add 
    + Bruno + Georgre, used for runtime analysis (usernames)

--- AntiVirtualization/UsernameCheck/UsernameCheck.go
+++ AntiVirtualization/UsernameCheck/UsernameCheck.go
@@ -8,7 +8,7 @@ import (
 // CheckForBlacklistedNames checks if the current username matches any blacklisted names.
 // It returns true if a blacklisted name is found, otherwise false.
 func CheckForBlacklistedNames() bool {
-	bn := []string{"Johnson", "Miller", "malware", "maltest", "CurrentUser", "Sandbox", "virus", "John Doe", "test user", "sand box", "WDAGUtilityAccount"}
+	bn := []string{"Johnson", "Miller", "malware", "maltest", "CurrentUser", "Sandbox", "virus", "John Doe", "test user", "sand box", "WDAGUtilityAccount", "Bruno", "george"}
 	user := strings.ToLower(os.Getenv("USERNAME"))
 	for _, name := range bn {
 		if user == strings.ToLower(name) {
