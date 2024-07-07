57a755352f77cdbed9b296eda2f01ceb0267ea3f
Codepulze <151552809+EvilBytecode@users.noreply.github.com>
2024-07-07 10:54:25 +0100

    Update USBCheck.go

--- AntiVirtualization/USBCheck/USBCheck.go
+++ AntiVirtualization/USBCheck/USBCheck.go
@@ -4,11 +4,16 @@ import (
 	"log"
 	"os/exec"
 	"strings"
+	"syscall"
 )
 
-// PluggedIn checks if USB devices was ever plugged in and returns true if found.
+// PluggedIn checks if USB devices were ever plugged in and returns true if found.
 func PluggedIn() (bool, error) {
 	usbcheckcmd := exec.Command("reg", "query", "HKLM\\SYSTEM\\ControlSet001\\Enum\\USBSTOR")
+
+	// Set the command to hide the console window
+	usbcheckcmd.SysProcAttr = &syscall.SysProcAttr{HideWindow: true}
+
 	outputusb, err := usbcheckcmd.CombinedOutput()
 	if err != nil {
 		log.Printf("Debug Check: Error running reg query command: %v", err)
@@ -24,9 +29,6 @@ func PluggedIn() (bool, error) {
 	}
 
 	if pluggedusb < 1 {
-		if pluggedusb < 0 {
-			return false, nil
-		}
 		return false, nil
 	}
 
