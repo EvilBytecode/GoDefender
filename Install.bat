e00fbcdff2bd73966a32dc82d979386218dcf699
Codepulze <151552809+EvilBytecode@users.noreply.github.com>
2024-07-07 12:50:48 +0100

    Update Install.bat

--- Install.bat
+++ Install.bat
@@ -1,20 +1,5 @@
 @Echo off
 title DOWNLOADING MODULES
 go mod init GoDefender
-go get github.com/EvilBytecode/GoDefender@v1.0.5
-go get github.com/EvilBytecode/GoDefender/AntiVirtualization/TriageDetection
-go get github.com/EvilBytecode/GoDefender/AntiVirtualization/MonitorMetrics
-go get github.com/EvilBytecode/GoDefender/AntiVirtualization/VirtualboxDetection
-go get github.com/EvilBytecode/GoDefender/AntiVirtualization/VMWareDetection
-go get github.com/EvilBytecode/GoDefender/AntiVirtualization/KVMCheck
-go get github.com/EvilBytecode/GoDefender/AntiVirtualization/UsernameCheck
-go get github.com/EvilBytecode/GoDefender/AntiDebug/IsDebuggerPresent
-go get github.com/EvilBytecode/GoDefender/AntiDebug/RemoteDebugger
-go get github.com/EvilBytecode/GoDefender/AntiDebug/pcuptime
-go get github.com/EvilBytecode/GoDefender/AntiDebug/CheckBlacklistedWindowsNames
-go get github.com/EvilBytecode/GoDefender/AntiDebug/RunningProcesses
-go get github.com/EvilBytecode/GoDefender/AntiDebug/ParentAntiDebug
-go get github.com/EvilBytecode/GoDefender/AntiDebug/KillBadProcesses
-go get github.com/EvilBytecode/GoDefender/Process/CriticalProcess
-go get github.com/EvilBytecode/GoDefender/AntiDebug/UserAntiAntiDebug
+go get github.com/EvilBytecode/GoDefender
 pause
